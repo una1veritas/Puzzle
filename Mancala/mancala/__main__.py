5202c8dd46c72fcb1fc41f2caed2b75dafe6768a
Sin Shimozono <una.veritas@me.com>
2025-02-06 10:22:14 +0900

    memo, main

--- Mancala/mancala/__main__.py
+++ Mancala/mancala/__main__.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
     swatch_start = time.time()
     game = Game(player_classes=[MinMaxPlayer, MinMaxPlayer], \
                 init_pieces_per_grid=2, \
-                grids_per_player=5
+                grids_per_player=6
                 )
     winner = game.run()
     swatch_stop = time.time()
