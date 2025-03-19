f621958af8faa534fabe02c6ab7cd4e09a0ac661
Sin Shimozono <una.veritas@icloud.com>
2025-03-19 13:49:51 +0900

    memo

--- Mancala/mancala/algorithm.py
+++ Mancala/mancala/algorithm.py
@@ -1,8 +1,14 @@
 from copy import deepcopy
 from typing import Dict, Tuple, List, Any
 import gc, json
+import psutil, os
 
 from board import Board2p
+
+def get_memory_usage():
+    process = psutil.Process(os.getpid())
+    memory_info = process.memory_info()
+    return memory_info.rss  # in bytes
 
 def search_with_min_max(player_id: int, board: Board2p, dp : dict) -> Tuple[int, int]:
     # if player_id != board.current_player() :
@@ -49,6 +55,8 @@ def search_with_min_max(player_id: int, board: Board2p, dp : dict) -> Tuple[int,
                 search_with_min_max.max_to_go = to_go
             print(f'max to over = {search_with_min_max.max_to_go}, dp size = {len(dp)}, signature = {search_with_min_max.max_sig}')
             print(f"key {str(board)} and value {result}.")
+            mem_usa = get_memory_usage()
+            print(f'memory usage {mem_usa/10245/1024:.2f}Mb.')
             print()
             gc.collect()
         dp[board] = result
